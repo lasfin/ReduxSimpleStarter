29751828049d37bef2c2036b916151a506763c45
lasfin <evgenij-kopica@yandex.ru>
2016-05-02 00:03:25 +0300

    VideoList and VideoListItem components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import API from './api_key';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const key = API.key;
+import API from './api_key';
+const API_KEY = API.key;
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    )
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({
+            key: API_KEY,
+            term: 'surfboards'
+        }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
